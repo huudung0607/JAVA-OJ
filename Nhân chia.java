086f04290e7e4fcd1a5bd11fa2ca6a3943cf2c6e
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-08-07 08:59:09 +0700

    Create Nhân chia.java

---
+++ Nhân chia.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class JavaApplication1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        System.out.println(a * 2 + "\n");
+        System.out.println(a * 10 + "\n");
+        System.out.println(a / 2 + "\n");
+        System.out.printf("%.3f",(double)a/2);
+        
+    }
+}
