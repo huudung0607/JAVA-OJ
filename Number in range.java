cb91bd0d4a2ed6c18e232779ff25027d5eb29e00
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-08-07 09:09:26 +0700

    Create Number in range.java

---
+++ Number in range.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class JavaApplication1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        long x = sc.nextLong();
+        long y = sc.nextLong();
+        System.out.println(1L * y - x + 1);
+    }
+}
