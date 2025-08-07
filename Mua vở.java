385f8b1a839f6a92782bef3722fea8f5eed14e7c
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-08-07 09:11:27 +0700

    Create Mua vở.java

---
+++ Mua vở.java
@@ -0,0 +1,12 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class JavaApplication1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        int x = sc.nextInt();
+        System.out.println("SO VO MUA DUOC LA : " + (n/x) + " !!!!!");
+    }
+}
